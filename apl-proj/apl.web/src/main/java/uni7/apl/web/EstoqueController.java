b0811a18f249e63a2aef21b0e98e0cafd80328b3
Nielsen Costa Teixeira <nielsenteixeira@gmail.com>
2019-04-19 23:00:56 -0300

    use Estoque from interface

--- apl-proj/apl.web/src/main/java/uni7/apl/web/EstoqueController.java
+++ apl-proj/apl.web/src/main/java/uni7/apl/web/EstoqueController.java
@@ -1,16 +1,19 @@
 package uni7.apl.web;
+import java.io.Serializable;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 
-import uni7.apl.ejb.EstoqueBean;
+import uni7.apl.ejb.Estoque;
 import uni7.apl.ejb.Produto;
 
 @SessionScoped
-public class EstoqueController {
+public class EstoqueController implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@EJB
-	EstoqueBean estoque;
+	Estoque estoque;
 	
 	public List<Produto> getEstoque() {
 		return estoque.getAll();
