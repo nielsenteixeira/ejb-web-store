8d14c12ba35549e04090594ee9253ea88efbc064
Nielsen Costa Teixeira <nielsenteixeira@gmail.com>
2019-04-20 18:37:29 -0300

    handle response format and handle exceptions

--- apl-proj/apl.web/src/main/java/uni7/apl/web/EstoqueServlet.java
+++ apl-proj/apl.web/src/main/java/uni7/apl/web/EstoqueServlet.java
@@ -1,5 +1,7 @@
 package uni7.apl.web;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -8,7 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpStatus;
+
 import com.google.gson.Gson;
+
+import uni7.apl.ejb.Produto;
 
 @WebServlet("/EstoqueServlet")
 public class EstoqueServlet extends HttpServlet {
@@ -17,21 +23,57 @@ public class EstoqueServlet extends HttpServlet {
 	@Inject EstoqueController controller;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		var acao = Acao.valueOf(request.getParameter("acao").toUpperCase());
+		request.getSession().invalidate();
+		
+		Acao acao = getAcao(request, response);
+		FormatoResposta formatoResposta = getFormatoResposta(request, response);
+		
+		PrintWriter out = response.getWriter();
 		
 		switch (acao) {
 		case LISTAR:
-			var estoque = new Gson().toJson(controller.getEstoque());
+			List<Produto> produtos = controller.getEstoque();
 			
-			var out = response.getWriter();
-	        response.setContentType("application/json");
-	        response.setCharacterEncoding("UTF-8");
-	        out.print(estoque);
-	        out.flush();   
-			break;
-
-		default:
+			
+			if(formatoResposta == FormatoResposta.JSON) {
+				String estoqueStr = new Gson().toJson(produtos);
+		        response.setContentType("application/json");
+		        response.setCharacterEncoding("UTF-8");
+		        out.print(estoqueStr);
+		        
+			} else {
+				HtmlBuilder htmlBuilder = new HtmlBuilder();
+				String htmlProdutos = htmlBuilder.buildHtmlList(produtos);
+				out.print(htmlProdutos);
+			}
+			
 			break;
 		}
+		
+		out.flush();
+	}
+
+	private FormatoResposta getFormatoResposta(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		if (request.getParameter("formato") != null) {
+			try	{
+				return FormatoResposta.valueOf(request.getParameter("formato").toUpperCase());
+			} catch (IllegalArgumentException e) {
+				response.sendError(HttpStatus.SC_BAD_REQUEST, "Escolha um formato válido: 'json' ou 'html'.");
+			}
+		} 
+		
+		return FormatoResposta.HTML;
+	}
+	
+	private Acao getAcao(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		if (request.getParameter("acao") != null) {
+			try {
+				return Acao.valueOf(request.getParameter("acao").toUpperCase());
+			} catch (IllegalArgumentException e) {
+				response.sendError(HttpStatus.SC_BAD_REQUEST, "Escolha uma ação válida: 'listar'.");
+			}
+		} 
+		
+		return Acao.LISTAR;
 	}
 }
---
+++ apl-proj/apl.web/src/main/java/uni7/apl/web/FormatoResposta.java
@@ -0,0 +1,17 @@
+package uni7.apl.web;
+
+public enum FormatoResposta {
+	JSON("json"),
+	HTML("html");
+	
+	private final String text;
+	
+	FormatoResposta(final String text) {
+        this.text = text;
+    }
+
+	@Override
+    public String toString() {
+        return text;
+    }
+}
