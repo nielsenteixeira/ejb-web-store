bdf4c4543881eb53efada5e3d839c1d93542bd46
Nielsen Costa Teixeira <nielsenteixeira@gmail.com>
2019-04-23 22:19:45 -0300

    minor refactor and extract method

--- apl-proj/apl.web/src/main/java/uni7/apl/web/util/HtmlBuilder.java
+++ apl-proj/apl.web/src/main/java/uni7/apl/web/util/HtmlBuilder.java
@@ -2,6 +2,7 @@ package uni7.apl.web.util;
 
 import java.util.List;
 
+import uni7.apl.ejb.compra.Carrinho;
 import uni7.apl.ejb.estocagem.Produto;
 
 public class HtmlBuilder {
@@ -19,25 +20,41 @@ public class HtmlBuilder {
 	public void setHtml(StringBuilder html) {
 		this.html = html;
 	}
+	
+	public String buildHtmlCarrinho(Carrinho carrinho) {
+		openHtml();
+		this.html.append("<h3>Compra finalizada!</h3>");
+		this.html.append("<h3>Valor total: + R$" + String.valueOf(carrinho.getPrecoCompra()) + "</h3>");
+		buildHtmlListProduto(carrinho.listarProdutos());
+		closeHtml();
+		return this.html.toString();
+	}
+	
+	private StringBuilder buildHtmlListProduto(List<Produto> produtos) {
+		this.html.append("<ul>");
+		produtos.forEach(produto -> {
+			this.html.append("<li>");
+			this.html.append("Código: " + produto.getCodigo() + " | ");
+			this.html.append("Produto: " + produto.getNome() + " | ");
+			this.html.append("Descricao: " + produto.getDescricao() + " | ");
+			this.html.append("Preço: " + produto.getPreco() + " | ");
+			this.html.append("Nome: " + produto.getNome() + " | ");
+			this.html.append("</li>");
+		});
+		this.html.append("</ul>");
+		return this.html;
+		
+	}
 
 	public String buildHtmlList(String tituloLista, List<Produto> produtos) {
 		openHtml();
 		
 		
-		if (produtos == null) {
+		if (produtos == null || produtos.isEmpty()) {
 			this.html.append("<h3>"+ tituloLista +" vazio!</h3>");
 		} else {
 			this.html.append("<h3>"+ tituloLista +"</h3>");
-			this.html.append("<ul>");
-			produtos.forEach(produto -> {
-				this.html.append("<li>");
-				this.html.append("Código: " + produto.getCodigo() + " | ");
-				this.html.append("Produto: " + produto.getNome() + " | ");
-				this.html.append("Descricao: " + produto.getDescricao() + " | ");
-				this.html.append("Preço: " + produto.getPreco() + " | ");
-				this.html.append("Nome: " + produto.getNome() + " | ");
-				this.html.append("</li>");
-			});
+			buildHtmlListProduto(produtos);
 		}
 		this.html.append("</ul>");
 		closeHtml();
